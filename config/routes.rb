48219b49f794e429a578bbeb6dc8da9a0156af71
wak10 <taka.watagashi422@gmail.com>
2021-12-21 09:45:49 +0000

    [add]ルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  scope module: :public do
+    root "homes#top"
+    resource :users, only:[:show, :edit, :update]
+    resources :grow_plants, except:[:edit]
+    post "/grow_plants/complete/:id" => "grow_plants#complete", as: "complete"
+  end
+
+  devise_for :users, skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+
+
+  namespace :admin do
+    resources :plants, only: [:index, :new, :create, :update, :edit]
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords],controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
