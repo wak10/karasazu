b65356e6465416648116715f7f46f37a31513779
wak10 <taka.watagashi422@gmail.com>
2021-12-23 14:11:52 +0000

    [add]植物追加機能

--- app/controllers/admin/plants_controller.rb
+++ app/controllers/admin/plants_controller.rb
@@ -1,18 +1,33 @@
 class Admin::PlantsController < ApplicationController
 
   def index
+    @plants = Plant.all
   end
 
   def new
+    @plant = Plant.new
   end
 
   def create
+    @plant = Plant.new(plant_params)
+    @plant.save
+    redirect_to admin_plants_path
   end
 
   def edit
+    @plant = Plant.find(params[:id])
   end
 
-  def updete
+  def update
+    @plant = Plant.find(params[:id])
+    @plant.update(plant_params)
+    redirect_to admin_plants_path
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:plant_name, :image, :frequency, :amount, :advice)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '429a5ee910bf0b17bb3a466664fc4241ff1d2796d4a64e1a6d36769336cdc194b7a9033262ee6571a40bd60f603f446b32ede280ba99541e5342d263ed970559'
