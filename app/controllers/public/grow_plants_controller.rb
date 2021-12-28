829eef55ee84d591455d6eb51ebddb1cf5115137
wak10 <taka.watagashi422@gmail.com>
2021-12-28 03:09:42 +0000

    [add]水やり完了ボタン追加

--- app/controllers/public/grow_plants_controller.rb
+++ app/controllers/public/grow_plants_controller.rb
@@ -9,23 +9,30 @@ class Public::GrowPlantsController < ApplicationController
     @grow_plant = GrowPlant.new
   end
 
-  def show
-    @grow_plant = GrowPlant.find(params[:id])
-  end
-
-  def complete
-  end
-
   def create
     @grow_plant = current_user.grow_plants.new(grow_plant_params)
     @grow_plant.save
     redirect_to  top_path
   end
 
+  def show
+    @grow_plant = GrowPlant.find(params[:id])
+    @log = Log.new
+  end
+
+  def complete
+    @log = Log.new(log_params)
+    @log.save
+    redirect_to action: :show
+  end
+
   def updete
   end
 
   def destroy
+    @grow_plant = GrowPlant.find(params[:id])
+    @grow_plant.destroy
+    redirect_to top_path
   end
 
   private
@@ -36,6 +43,10 @@ class Public::GrowPlantsController < ApplicationController
 
   def grow_plant_params
     params.require(:grow_plant).permit( :plant_id, :user_id, :nick_name)
+  end
+
+  def log_params
+    params.require(:log).permit( :grow_plant_id, :user_id)
   end
 
 end
