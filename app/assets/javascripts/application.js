a6bf5a5031682a7152cc6be7524bf3e070da7d71
wak10 <taka.watagashi422@gmail.com>
2022-01-13 01:00:46 +0000

    [add]userトップ画面レイアウト調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,35 +17,3 @@
 //= require jquery_ujs
 //= require jquery.jposta
 //= require_tree .
-
-$(document).on("turbolinks:load", function(){
-	// admin/items#new,editでの画像プレビュー
-	function readURL(input) {
-		if(input.files && input.files[0]){
-			var reader = new FileReader();
-			reader.onload = function (e) {
-				$('#img_prev').attr('src', e.target.result);
-			}
-			reader.readAsDataURL(input.files[0]);
-		}
-	}
-	$("#item_image").change(function(){
-		readURL(this);
-	});
-	// public/orders#confirmでの確認ダイアログ
-	$('.submit_to_create_order').on('click', function(){
-			var result = window.confirm('OKを押すと、注文が確定します。');
-			if(!result){
-				return false;
-			}
-	});
-});
-
-$(document).on('turbolinks:load', function () {
-	$('.jpostal_code').jpostal({
-			postcode: ['.jpostal_code'],
-			address: {
-				'.jpostal_address': '%3%4%5',
-			},
-		})
-});
