cd9b6b083e94933c6041f1e9314f6f746cbf10f7
wak10 <taka.watagashi422@gmail.com>
2022-01-14 01:04:34 +0000

    [add]育成植物追加機能修正

--- app/models/grow_plant.rb
+++ app/models/grow_plant.rb
@@ -5,8 +5,6 @@ class GrowPlant < ApplicationRecord
 
   enum place:{ 日向:0, 半日陰:1 }
 
-  validates :nick_name, presence: true
-
   def self.send_mail
     @grow_plants = GrowPlant.all
     @grow_plants.find_each do |grow_plant|
@@ -24,7 +22,7 @@ class GrowPlant < ApplicationRecord
       end
     end
   end
-  
+
 end
 
 
