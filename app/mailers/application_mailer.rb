631f448de98784736030881b91c2a46ea920d872
wak10 <taka.watagashi422@gmail.com>
2022-02-01 13:16:14 +0000

    [fix]mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '水やり通知<pf.test939@gmail.com>',
+  default from: 'pf.test939@gmail.com',
   bcc: "pf.test939@gmail.com"
   layout 'mailer'
 end
