7b87c407b90090b9c54dbacbb55c13831fe349ab
wak10 <taka.watagashi422@gmail.com>
2022-02-01 12:52:27 +0000

    [update]バッチ処理修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
   require File.expand_path(File.dirname(__FILE__) + "/environment")
-  rails_env = Rails.env.to_sym
-  set :environment, rails_env
+
+  set :environment, :production
   set :output, 'log/cron.log'
 
 
-  every :day, at: "0:00 am" do
+  every :day, at: "21:55 pm" do
     runner "GrowPlant.send_mail"
   end
