9ac72ffd2a6b3f6c73615fa9278d40f4662cf4f4
wak10 <taka.watagashi422@gmail.com>
2022-02-02 11:48:24 +0000

    [fix]

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,11 @@
   set :environment, :production
   set :output, 'log/cron.log'
 
+  def jst(time)
+    Time.zone = 'Asia/Tokyo'
+    Time.zone.parse(time).localtime($system_utc_offset)
+  end
 
-  every :day, at: "8:30 pm" do
+  every :day, at: "8:50 pm" do
     runner "GrowPlant.send_mail"
   end
